72942fb82fbd2db4b87253f6636bdcb34a633eab
Fardeen S <fardeenclan@gmail.com>
2024-01-23 12:03:53 +0530

    added somethings

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,42 @@
+import Card from "@/components/Card";
+import { Button, buttonVariants } from "@/components/ui/button";
 import React from "react";
 
+const btnLinks = [
+  {
+    title: "All Products",
+  },
+  {
+    title: "Mobiles",
+  },
+  {
+    title: "Laptops",
+  },
+  {
+    title: "Tablets",
+  },
+  {
+    title: "Television",
+  },
+];
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-row">
+      <div className="w-4/12">Sidebar</div>
+      <div className="flex flex-col">
+        <h1 className="text-2xl mt-3">Recommended</h1>
+        <div className="flex flex-row gap-2 mt-3">
+          {btnLinks.map((link) => (
+            <Button variant={"outline"}>{link.title}</Button>
+          ))}
+        </div>
+        <div className="flex mt-10">
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
